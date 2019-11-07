2121331db4f7e27317501ef3da84eec6fb34e92a
Stephen Jayakar <aj12ay@gmail.com>
2019-11-06 17:06:54 -0800

    basic login functionality

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
 use tonic::{transport::Server, Request, Response, Status};
+use std::collections::HashSet;
+use tokio::sync::Mutex;
 
 const SERVER_PORT: i32 = 50051;
 
@@ -9,9 +11,12 @@ pub mod rust_chat {
 use rust_chat::{
   server::{ChatRoom, ChatRoomServer},
   LoginRequest, LoginReply,
+  SendMessageRequest, SendMessageReply,
 };
 
-pub struct MyChatRoom {}
+pub struct MyChatRoom {
+  users: Mutex::<HashSet<String>>,
+}
 
 #[tonic::async_trait]
 impl ChatRoom for MyChatRoom {
@@ -19,18 +24,31 @@ impl ChatRoom for MyChatRoom {
     &self,
     request: Request<LoginRequest>
   ) -> Result<Response<LoginReply>, Status> {
-    let reply = rust_chat::LoginReply {
-      ok: false.into()
-    };
+    let mut users = self.users.lock().await;
+    let username = request.into_inner().username;
+    let success = users.insert(username.clone());
+    if success {
+      println!("User {} logged on!", username);
+    }
 
+    let reply = rust_chat::LoginReply {
+      ok: success,
+    };
     Ok(Response::new(reply))
+  }
+
+  async fn send_message(
+    &self,
+    request: Request<SendMessageRequest>
+  ) -> Result<Response<SendMessageReply>, Status> {
+    unimplemented!();
   }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
   let addr = format!("[::1]:{}", SERVER_PORT).parse()?;
-  let chatroom = MyChatRoom {};
+  let chatroom = MyChatRoom { users: Mutex::new(HashSet::new()) };
 
   println!("Server listening in on port {}", SERVER_PORT);
 
